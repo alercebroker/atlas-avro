2a51bf37050afcab018ef2fa49f65d9c38bcd29c
Daniela <druzmieres@gmail.com>
2019-10-16 16:18:06 -0300

    reading schema from file

--- schema.go
+++ schema.go
@@ -4,71 +4,24 @@ import (
   "encoding/json"
   "fmt"
   "github.com/khezen/avro"
+  "io/ioutil"
 )
 
-/* Column names: RA,Dec,mag,dmag,x,y,major,minor,phi,det,chi/N,Pvr,Ptr,Pmv,
-   Pkn,Pno,Pbn,Pcr,Pxt,Psc,Dup,WPflx,dflx */
 func main() {
-  mainSchema := []byte(
-    `{
-      "namespace": "atlas",
-      "type": "record",
-      "name": "alert",
-      "doc": "avro schema for ATLAS alerts",
-      "version": "0.1",
-      "fields": [
-      {"name": "schemavsn", "type": "string", "doc": "schema version used"},
-      {"name": "RA", "type": "double", "doc": "RA"},
-      {"name": "Dec", "type": "double", "doc": "Dec"},
-      {"name": "mag", "type": "double", "doc": "mag"},
-      {"name": "dmag", "type": "double", "doc": "dmag"},
-      {"name": "x", "type": "double", "doc": "x"},
-      {"name": "y", "type": "double", "doc": "y"},
-      {"name": "major", "type": "double", "doc": "major"},
-      {"name": "minor", "type": "double", "doc": "minor"},
-      {"name": "phi", "type": "double", "doc": "phi"},
-      {"name": "det", "type": "double", "doc": "det"},
-      {"name": "chi/N", "type": "double", "doc": "chi/N"},
-      {"name": "Pvr", "type": "double", "doc": "Pvr"},
-      {"name": "Ptr", "type": "double", "doc": "Ptr"},
-      {"name": "Pmv", "type": "double", "doc": "Pmv"},
-      {"name": "Pkn", "type": "double", "doc": "Pkn"},
-      {"name": "Pno", "type": "double", "doc": "Pno"},
-      {"name": "Pbn", "type": "double", "doc": "Pbn"},
-      {"name": "Pcr", "type": "double", "doc": "Pcr"},
-      {"name": "Pxt", "type": "double", "doc": "Pxt"},
-      {"name": "Psc", "type": "double", "doc": "Psc"},
-      {"name": "Dup", "type": "double", "doc": "Dup"},
-      {"name": "WPflx", "type": "double", "doc": "WPflx"},
-      {"name": "dflx", "type": "double", "doc": "dflx"},
-      {"name": "cutoutScience", "type": ["atlas.alert.cutout", "null"], "default": null},
-      {"name": "cutoutTemplate", "type": ["atlas.alert.cutout", "null"], "default": null},
-      {"name": "cutoutDifference", "type": ["atlas.alert.cutout", "null"], "default": null}
-      ]}`
-    )
+  // Read AVRO file as string
+  mainSchema, err := ioutil.ReadFile("alert.avsc")
 
-      cutoutSchema := []byte(
-        `{
-          "namespace": "atlas.alert",
-          "type": "record",
-          "name": "cutout",
-          "doc": "avro schema for ATLAS cutouts",
-          "version": "0.1",
-          "fields": [
-          {"name": "fileName", "type": "string"},
-          {"name": "stampData", "type": "bytes", "doc": "fits.gz"}
-          ]}`
-        )
+  //mainSchema, err := avro.ParseSchemaFile("alert.avsc")
 
   // Unmarshal JSON  bytes to Schema interface
-  var anySchema avro.AnySchema
+/*  var anySchema avro.AnySchema
   err := json.Unmarshal(mainSchema, &anySchema)
   if err != nil {
     panic(err)
   }
-  schema := anySchema.Schema()
+  schema := anySchema.Schema()*/
   // Marshal Schema interface to JSON bytes
-  mainSchema, err = json.Marshal(schema)
+  mainSchema, err = json.Marshal(mainSchema)
   if err != nil {
     panic(err)
   }
