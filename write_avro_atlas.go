79e47f213136c09527d313b94be9039d9c256ce5
Daniela <druzmieres@gmail.com>
2019-10-17 16:09:20 -0300

    hardcoded test

--- write_avro_atlas.go
+++ write_avro_atlas.go
@@ -9,11 +9,6 @@ import (
 )
 
 type AtlasRecord struct {
-        A int64  `avro:"a"`
-        B string `avro:"b"`
-}
-
-type AtlasRecord struct {
   schemavsn string `avro:"schemavsn"`
   RA  float64 `avro:"RA"`
   Dec  float64 `avro:"Dec"`
@@ -25,7 +20,7 @@ type AtlasRecord struct {
   minor  float64 `avro:"minor"`
   phi  float64 `avro:"phi"`
   det  float64 `avro:"det"`
-  chi/N  float64 `avro:"chi/N"`
+  chi  float64 `avro:"chi/N"`
   Pvr  float64 `avro:"Pvr"`
   Ptr  float64 `avro:"Ptr"`
   Pmv  float64 `avro:"Pmv"`
@@ -45,17 +40,33 @@ type AtlasRecord struct {
 
 type Cutout struct {
   fileName string
-  stampData bytes //fits.gz
+  stampData []byte // bytes //fits.gz
 }
 
 func main() {
+  // Create cutouts
+  var p_cutoutScience *Cutout
+  p_cutoutScience = new(Cutout)
+  p_cutoutScience.fileName = "candid820127160015015005_pid820127160015_targ_sci.fits.gz"
+  p_cutoutScience.stampData = []byte{0x61, 0x62, 0x43}
+
+  var p_cutoutTemplate *Cutout
+  p_cutoutTemplate = new(Cutout)
+  p_cutoutTemplate.fileName = "candid820127160015015005_pid820127160015_targ_tem.fits.gz"
+  p_cutoutTemplate.stampData = []byte{0x61, 0x63, 0x10}
+
+  var p_cutoutDifference *Cutout
+  p_cutoutDifference = new(Cutout)
+  p_cutoutDifference.fileName = "candid820127160015015005_pid820127160015_targ_dif.fits.gz"
+  p_cutoutDifference.stampData = []byte{0x21, 0x67, 0x20}
+
   // Parse the schema file
-  schema, err := avro.ParseSchemaFile("alert_simple.avsc")
+  schema, err := avro.ParseSchemaFile("alert.avsc")
   if err != nil {
     log.Fatal(err)
   }
   // Open file to write to
-  f, err := os.Create("file.avro")
+  f, err := os.Create("alert.avro")
   if err != nil {
     fmt.Println(err)
     return
@@ -66,10 +77,38 @@ func main() {
   // Create DatumWriter and set schema
   datumWriter := avro.NewSpecificDatumWriter()
   datumWriter.SetSchema(schema)
-  // Create a simple record
-  simple := &SimpleRecord{A: 27, B: "foo",}
+  // Create an ATLAS record
+  atlas_record := &AtlasRecord{
+    schemavsn: "0.1",
+    RA: 261.09578,
+    Dec: 45.54479,
+    mag: 14.806,
+    dmag: 0.18,
+    x: 1000.9,
+    y: 29.19,
+    major: 2.27,
+    minor: 1.97,
+    phi: 128.2,
+    det: 0,
+    chi: 0.23,
+    Pvr: 999,
+    Ptr: 0,
+    Pmv: 0,
+    Pkn: 0,
+    Pno: 0,
+    Pbn: 0,
+    Pcr: 0,
+    Pxt: 0,
+    Psc: 0,
+    Dup: 1,
+    WPflx: 78875.5,
+    dflx: 3.4,
+    cutoutScience: p_cutoutScience,
+    cutoutTemplate: p_cutoutTemplate,
+    cutoutDifference: p_cutoutDifference,
+  }
   // Write the data to the buffer through datumWriter
-  err = datumWriter.Write(simple, encoder)
+  err = datumWriter.Write(atlas_record, encoder)
   if err != nil {
     log.Fatal(err)
   }
@@ -79,7 +118,7 @@ func main() {
     log.Fatal(err)
   }
   // fileWriter needs an argument
-  err = fileWriter.Write(simple)
+  err = fileWriter.Write(atlas_record)
   err = fileWriter.Flush()
   if err != nil {
     fmt.Println(err)
