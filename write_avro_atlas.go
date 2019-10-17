a2d2d02c1b317f477a3b0c278bafbe7a46351397
Daniela <druzmieres@gmail.com>
2019-10-17 14:25:39 -0300

    basic atlas alert struct

---
+++ write_avro_atlas.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+  "bytes"
+  "fmt"
+  "gopkg.in/avro.v0"
+  "log"
+  "os"
+)
+
+type AtlasRecord struct {
+        A int64  `avro:"a"`
+        B string `avro:"b"`
+}
+
+type AtlasRecord struct {
+  schemavsn string `avro:"schemavsn"`
+  RA  float64 `avro:"RA"`
+  Dec  float64 `avro:"Dec"`
+  mag  float64 `avro:"mag"`
+  dmag  float64 `avro:"dmag"`
+  x  float64 `avro:"x"`
+  y  float64 `avro:"y"`
+  major  float64 `avro:"major"`
+  minor  float64 `avro:"minor"`
+  phi  float64 `avro:"phi"`
+  det  float64 `avro:"det"`
+  chi/N  float64 `avro:"chi/N"`
+  Pvr  float64 `avro:"Pvr"`
+  Ptr  float64 `avro:"Ptr"`
+  Pmv  float64 `avro:"Pmv"`
+  Pkn  float64 `avro:"Pkn"`
+  Pno  float64 `avro:"Pno"`
+  Pbn  float64 `avro:"Pbn"`
+  Pcr  float64 `avro:"Pcr"`
+  Pxt  float64 `avro:"Pxt"`
+  Psc  float64 `avro:"Psc"`
+  Dup  float64 `avro:"Dup"`
+  WPflx  float64 `avro:"WPflx"`
+  dflx  float64 `avro:"dflx"`
+  cutoutScience ["atlas.alert.cutout", "null"]
+  cutoutTemplate ["atlas.alert.cutout", "null"]
+  cutoutDifference ["atlas.alert.cutout", "null"]
+}
+
+func main() {
+  // Parse the schema file
+  schema, err := avro.ParseSchemaFile("alert_simple.avsc")
+  if err != nil {
+    log.Fatal(err)
+  }
+  // Open file to write to
+  f, err := os.Create("file.avro")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  // Create buffer to store data
+  var buf bytes.Buffer
+  encoder := avro.NewBinaryEncoder(&buf)
+  // Create DatumWriter and set schema
+  datumWriter := avro.NewSpecificDatumWriter()
+  datumWriter.SetSchema(schema)
+  // Create a simple record
+  simple := &SimpleRecord{A: 27, B: "foo",}
+  // Write the data to the buffer through datumWriter
+  err = datumWriter.Write(simple, encoder)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // Create a fileWriter
+  fileWriter, err := avro.NewDataFileWriter(f, schema, datumWriter)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // fileWriter needs an argument
+  err = fileWriter.Write(simple)
+  err = fileWriter.Flush()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  // Close the file
+  fileWriter.Close()
+}
