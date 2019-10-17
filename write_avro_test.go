3d8705164c030aac913b83beac7c3b3d2591b73d
Daniela <druzmieres@gmail.com>
2019-10-17 11:04:16 -0300

    write sample avro

---
+++ write_avro_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+  "bytes"
+  "fmt"
+  "gopkg.in/avro.v0"
+  "log"
+  "os"
+)
+
+type SimpleRecord struct {
+        A int64  `avro:"a"`
+        B string `avro:"b"`
+}
+
+func main() {
+  // Parse the schema file
+  schema, err := avro.ParseSchemaFile("alert-test.avsc")
+  if err != nil {
+    log.Fatal(err)
+  }
+  // Open file to write to
+  f, err := os.Create("file.avro")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  // Create buffer to store data
+  var buf bytes.Buffer
+  encoder := avro.NewBinaryEncoder(&buf)
+  // Create DatumWriter
+  datumWriter := avro.NewSpecificDatumWriter()
+  datumWriter.SetSchema(schema)
+  // Create a simple record
+  simple := &SimpleRecord{A: 27, B: "foo",}
+  // Write the data to the buffer through datumWriter
+  err = datumWriter.Write(simple, encoder)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // Create a fileWriter
+  fileWriter, err := avro.NewDataFileWriter(f, schema, datumWriter)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // fileWriter needs an argument
+  err = fileWriter.Write(simple)
+  err = fileWriter.Flush()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  // Close the file
+  fileWriter.Close()
+}
