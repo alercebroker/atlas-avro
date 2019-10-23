a7a88ce44f31492627cb34ba4a947b54bafcf7ec
Daniela <druzmieres@gmail.com>
2019-10-22 22:33:17 -0300

    main outline

---
+++ atlas_main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "os"
+
+func main() {
+  // main outline
+  // get ddc/other format file
+  // get the headers or find them in the file
+  // read file data (in chunks?)
+  // create avro files
+
+  // produce to topic
+}
