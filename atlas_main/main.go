79fdf94a944cfa4d2d2997cdb3f44b93f1f089f6
Daniela <druzmieres@gmail.com>
2019-10-28 09:24:36 -0300

    basic case

--- atlas_main/main.go
+++ atlas_main/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "os"
+import (
+  "fmt"
+  "os"
+)
 
 func main() {
-  // main outline
-  // get ddc/other format file
-  // get the headers or find them in the file
-  // read file data (in chunks?)
-  // create avro files
+  // Get program arguments
+  if len(os.Args) > 2 {
+    programArguments := os.Args[1:] // Could be a directory where the ddc files are.
+    // get ddc/other format file
 
-  // produce to topic
+    // get the headers or find them in the file
+    // read file data (in chunks?)
+    // create avro files
+
+    // produce to topic
+  }
+  else {
+    fmt.Println("No arguments given.")
+  }
 }
