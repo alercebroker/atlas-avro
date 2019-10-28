86f3834ff58d21f50a13e24222f437039db52e4d
Daniela <druzmieres@gmail.com>
2019-10-28 12:00:10 -0300

    get list of files

--- atlas_main/main.go
+++ atlas_main/main.go
@@ -2,22 +2,40 @@ package main
 
 import (
   "fmt"
+  "log"
+  //"io/ioutil"
   "os"
+  "path/filepath"
 )
+
+func FilePathWalkDir(root string) ([]string, error) {
+  var files []string
+  err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+    if !info.IsDir() {
+      files = append(files, path)
+    }
+    return nil
+  })
+  return files, err
+}
 
 func main() {
   // Get program arguments
-  if len(os.Args) > 2 {
-    programArguments := os.Args[1:] // Could be a directory where the ddc files are.
-    // get ddc/other format file
-
+  if len(os.Args) > 1 {
+    programArguments := os.Args[1:] // Could be a directory or something else
+    // Read files in directory
+    directory := programArguments[0]
+    // Files is a list of the files in the directory
+    files, err := FilePathWalkDir(directory)
+    if err != nil {
+      panic(err)
+    }
     // get the headers or find them in the file
     // read file data (in chunks?)
     // create avro files
 
     // produce to topic
-  }
-  else {
-    fmt.Println("No arguments given.")
+  }else{
+    log.Fatal("No arguments given.")
   }
 }
